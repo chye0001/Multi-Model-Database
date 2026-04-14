5c6aeea16b1012b3f38395f679f99ae412135472
PaulSebastianKrus <paulsebastiankrus@gmail.com>
2026-04-14 20:11:42 +0200

    fixes review implementation

--- routes/outfit.router.ts
+++ routes/outfit.router.ts
@@ -2,12 +2,19 @@ import { Router } from "express";
 import { outfitRepositoryFactory } from "../repositories/factories/OutfitRepositoryFactory.js";
 import { OutfitService } from "../services/OutfitService.js";
 import { OutfitController } from "../controllers/OutfitController.js";
+import { reviewRepositoryFactory } from "../repositories/factories/ReviewRepositoryFactory.js";
+import { ReviewService } from "../services/ReviewService.js";
+import { ReviewController } from "../controllers/ReviewController.js";
 
 const router = Router();
 
 const outfitRepository = outfitRepositoryFactory();
 const outfitService = new OutfitService(outfitRepository);
 const outfitController = new OutfitController(outfitService);
+
+const reviewRepository = reviewRepositoryFactory();
+const reviewService = new ReviewService(reviewRepository);
+const reviewController = new ReviewController(reviewService);
 
 router.get("/", outfitController.getAllOutfits);
 router.get("/:id", outfitController.getOutfitById);
@@ -18,5 +25,7 @@ router.delete("/:id", outfitController.deleteOutfit);
 router.get("/:id/items", outfitController.getOutfitItems);
 router.post("/:id/items", outfitController.addItemToOutfit);
 router.delete("/:id/items/:itemId", outfitController.removeItemFromOutfit);
+
+router.get("/:id/reviews", reviewController.getReviewsByOutfitId);
 
 export default router;
